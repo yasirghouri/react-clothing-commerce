8f5c07de1edaec456fdf34baf7320c60522c7fa1
yasirghouri <yasir.ag7@gmail.com>
2021-02-04 12:00:46 +0500

    added MenuItem Component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import Homepage from "./components/Homepage/Homepage";
+import Homepage from "./pages/Homepage/Homepage";
 
 function App() {
   return (
---
+++ src/components/Directory/Directory.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+import "./Directory.scss";
+
+export class Directory extends Component {
+  render() {
+    return <div></div>;
+  }
+}
+
+export default Directory;

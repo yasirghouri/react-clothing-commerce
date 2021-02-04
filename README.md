22e1ac1d5e1373b2fb2032b2afdf2d087b028203
yasirghouri <yasir.ag7@gmail.com>
2021-02-04 15:14:48 +0500

    implementing react routing

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 ## Dependency Packages:
 
 1. npm i node-sass
+2. npm i react-router-dom
--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
+import { Route, Switch } from "react-router-dom";
+
 import "./App.css";
 import Homepage from "./pages/Homepage/Homepage";
 
 function App() {
   return (
     <div>
-      <Homepage />
+      <Switch>
+        <Route exact path="/" component={Homepage} />
+      </Switch>
     </div>
   );
 }

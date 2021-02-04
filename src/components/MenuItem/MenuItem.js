4d084f431f6b17e185f77a7c9f792787e75ee899
yasirghouri <yasir.ag7@gmail.com>
2021-02-04 13:20:36 +0500

    Added Directory Component

--- src/components/MenuItem/MenuItem.js
+++ src/components/MenuItem/MenuItem.js
@@ -2,11 +2,17 @@ import React from "react";
 
 import "./MenuItem.scss";
 
-const MenuItem = ({ title }) => {
+const MenuItem = ({ title, imageUrl, size }) => {
   return (
-    <div className="menu-item">
+    <div className={`${size} menu-item`}>
+      <div
+        className="background-image"
+        style={{
+          backgroundImage: `url(${imageUrl})`,
+        }}
+      />
       <div className="content">
-        <h1 className="title">{title}</h1>
+        <h1 className="title">{title.toUpperCase()}</h1>
         <span className="sub-title">SHOP NOW</span>
       </div>
     </div>
--- src/pages/Homepage/Homepage.js
+++ src/pages/Homepage/Homepage.js
@@ -1,42 +1,12 @@
 import React from "react";
 
+import Directory from "../../components/Directory/Directory";
 import "./Homepage.scss";
 
 const Homepage = () => {
   return (
     <div className="homepage">
-      <div className="directory-menu">
-        <div className="menu-item">
-          <div className="content">
-            <h1 className="title">HATS</h1>
-            <span className="sub-title">SHOP NOW</span>
-          </div>
-        </div>
-        <div className="menu-item">
-          <div className="content">
-            <h1 className="title">JACKETS</h1>
-            <span className="sub-title">SHOP NOW</span>
-          </div>
-        </div>
-        <div className="menu-item">
-          <div className="content">
-            <h1 className="title">SNEAKERS</h1>
-            <span className="sub-title">SHOP NOW</span>
-          </div>
-        </div>
-        <div className="menu-item">
-          <div className="content">
-            <h1 className="title">WOMEN</h1>
-            <span className="sub-title">SHOP NOW</span>
-          </div>
-        </div>
-        <div className="menu-item">
-          <div className="content">
-            <h1 className="title">MENS</h1>
-            <span className="sub-title">SHOP NOW</span>
-          </div>
-        </div>
-      </div>
+      <Directory />
     </div>
   );
 };

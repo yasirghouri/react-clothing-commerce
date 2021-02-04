ad34a7eb16c9374918f9e5b1b99016a63f0ed659
yasirghouri <yasir.ag7@gmail.com>
2021-02-04 15:53:01 +0500

    added CollectionPreview Component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import { Route, Switch } from "react-router-dom";
 
 import "./App.css";
-import Homepage from "./pages/Homepage/Homepage";
+import HomePage from "./pages/Homepage/Homepage";
+import ShopPage from "./pages/Shop/Shop";
 
 function App() {
   return (
     <div>
       <Switch>
-        <Route exact path="/" component={Homepage} />
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/shop" component={ShopPage} />
       </Switch>
     </div>
   );
---
+++ src/pages/Shop/Shop.js
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+import CollectionPreview from "../../components/CollectionPreview/CollectionPreview";
+
+import Shop_Data from "./Shop.Data";
+import "./Shop.scss";
+
+export class Shop extends Component {
+  state = {
+    collections: Shop_Data,
+  };
+  render() {
+    const { collections } = this.state;
+    return (
+      <div className="shop-page">
+        {collections.map(({ id, ...otherCollectionProps }) => (
+          <CollectionPreview key={id} {...otherCollectionProps} />
+        ))}
+      </div>
+    );
+  }
+}
+
+export default Shop;

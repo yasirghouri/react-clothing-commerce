5e62c223b67db9cad56e2da6fa6475dc145b1351
yasirghouri <yasir.ag7@gmail.com>
2021-02-09 12:41:42 +0500

    added SignUp Component and sign up with email and password functionalities

--- src/pages/Auth/Auth.js
+++ src/pages/Auth/Auth.js
@@ -1,5 +1,6 @@
 import React from "react";
 import SignIn from "../../components/SignIn/SignIn";
+import SignUp from "../../components/SignUp/SignUp";
 
 import "./Auth.scss";
 
@@ -7,6 +8,7 @@ const Auth = () => {
   return (
     <div className="auth">
       <SignIn />
+      <SignUp />
     </div>
   );
 };

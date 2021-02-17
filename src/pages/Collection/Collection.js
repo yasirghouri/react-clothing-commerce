cb6c097a3498e07c382455d72310f80cd90ca4d1
yasirghouri <yasir.ag7@gmail.com>
2021-02-17 15:01:02 +0500

    converted Collection page to styled-components

--- src/pages/Collection/Collection.js
+++ src/pages/Collection/Collection.js
@@ -3,19 +3,23 @@ import { connect } from "react-redux";
 
 import { selectCollection } from "../../redux/shop/shopSelectors";
 import CollectionItem from "../../components/CollectionItem/CollectionItem";
-import "./Collection.scss";
+import {
+  CollectionContainer,
+  TitleContainer,
+  ItemsContainer,
+} from "./CollectionStyles";
 
 const Collection = ({ collection }) => {
   const { title, items } = collection;
   return (
-    <div className="collection-page">
-      <h2 className="title">{title}</h2>
-      <div className="items">
+    <CollectionContainer>
+      <TitleContainer>{title}</TitleContainer>
+      <ItemsContainer>
         {items.map((item) => (
           <CollectionItem key={item.id} item={item} />
         ))}
-      </div>
-    </div>
+      </ItemsContainer>
+    </CollectionContainer>
   );
 };
 

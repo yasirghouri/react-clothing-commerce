4b0d8696048aa6412d4c322b82f85a9386c3e8ad
yasirghouri <yasir.ag7@gmail.com>
2021-02-25 14:14:04 +0500

    added nodejs express backend for stripe payments in our app

---
+++ client/src/components/StripeCheckoutButton/StripeCheckoutButton.js
@@ -0,0 +1,44 @@
+import React from "react";
+import axios from "axios";
+
+import StripeCheckout from "react-stripe-checkout";
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey = process.env.REACT_APP_STRIPE_PUBLISHABLE_KEY;
+  const onToken = (token) => {
+    axios({
+      url: "payment",
+      method: "post",
+      data: {
+        amount: priceForStripe,
+        token: token,
+      },
+    })
+      .then((response) => {
+        alert("Payment Successful");
+      })
+      .catch((error) => {
+        console.log("Payment Error: ", error);
+        alert(
+          "There was an issue with your payment! Please make sure you use the provided credit card."
+        );
+      });
+  };
+  return (
+    <StripeCheckout
+      label="Pay Now"
+      name="Ghouri's Clothing"
+      billingAddress
+      shippingAddress
+      image="https://svgshare.com/i/CUz.svg"
+      description={`Your total price is ${price}`}
+      amount={priceForStripe}
+      panelLabel="Pay Now"
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;

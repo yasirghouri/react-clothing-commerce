66f40cca6e926628728c1c3ed06af97a2e0fce56
yasirghouri <yasir.ag7@gmail.com>
2021-03-01 09:24:10 +0500

    added styles to make app mobile responsive

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,17 @@
-import React, { useEffect } from "react";
+import React, { useEffect, lazy } from "react";
 import { Route, Switch, Redirect } from "react-router-dom";
 import { connect } from "react-redux";
 import { createStructuredSelector } from "reselect";
 
-import "./App.css";
 import Header from "./components/Header/Header";
 import AuthPage from "./pages/Auth/Auth";
-import HomePage from "./pages/Homepage/Homepage";
 import ShopPage from "./pages/Shop/Shop";
 import CheckoutPage from "./pages/Checkout/Checkout";
 import { selectCurrentUser } from "./redux/user/userSelectors";
 import { checkUserSession } from "./redux/user/userActions";
+import GlobalStyles from "./GlobalStyles";
+
+const HomePage = lazy(() => import("./pages/Homepage/Homepage"));
 
 const App = ({ checkUserSession, currentUser }) => {
   useEffect(() => {
@@ -18,6 +19,7 @@ const App = ({ checkUserSession, currentUser }) => {
   }, [checkUserSession]);
   return (
     <div>
+      <GlobalStyles />
       <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
